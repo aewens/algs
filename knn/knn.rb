8393e096ef5a57ececf0e882f96404ae4b62fc81
aewens <aewenscript@gmail.com>
2014-12-12 13:46:44 -0600

    Added documentation comments

--- knn/knn.rb
+++ knn/knn.rb
@@ -9,22 +9,27 @@ def distance i, j
     j
 end
 
+# Do distance a bunch
 def distances i, js
     js.map{|j| distance(i,j)}
 end
 
+# Find the nearest distances
 def near xs, k
     xs.sort_by{|k|k[:dist]}.take(k).map{|j| j[:type]}
 end
 
+# Guess what the type of `i` is
 def guess xs
     xs.group_by{|x| x}.values.max_by(&:size)[0]
 end
 
+# Shortcut to using all the other functions
 def knn i, js, k
     guess(near(distances(i, js), k))
 end
 
+# Test data
 i = {:type=>nil, :xy=>[1,2]}
 js = [
     {:type=>0, :xy=>[3,2]},
@@ -36,6 +41,4 @@ js = [
 ]
 
 i[:type] = knn i, js, 2
-p i
-
-# p i
+p i #=> Should be type 1
