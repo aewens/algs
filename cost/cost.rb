1e3ad48c168d26b137745d84b2d0405806a7e3fa
aewens <aewenscript@gmail.com>
2014-12-13 14:36:39 -0600

    Added examples to cost function

--- cost/cost.rb
+++ cost/cost.rb
@@ -1,17 +1,24 @@
 #!/usr/bin/env ruby
 
-xs = []
-ys = []
+xs = [0,1,2,3,4,5]
+ys = [3,2,4,1,5,0]
 
 # Guess what `y` will be in respect to `x`
-def hypoth x, O0, O1
-    O0 + (O1 * x)
+def hypoth x, o0, o1
+    o0 + (o1 * x)
 end
 
 # Squared error
-def cost xs, ys
+def cost xs, ys, o0, o1
     return unless xs.size == ys.size
     size = xs.size
-    sqsum = (0..size-1).reduce(0){|ret, i| ret + (hypoth(xs[i]) - ys[i])**2}
-    (1 / (2 * size)) * sqsum
+    sqsum = (0..size-1).reduce(0) do |ret, i|
+        ret + (hypoth(xs[i],o0,o1) - ys[i])**2
+    end
+    (1.0 / (2 * size)) * sqsum
 end
+
+o0 = 0
+o1 = 0
+
+p cost xs, ys, o0, o1
